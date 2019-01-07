351806194bd87519f70ca412dc129056419ccf48
Brooke E Kline Jr <brooke.kline@moov.io>
2019-01-07 14:56:53 -0500

    Remove copied ParseError and ErrorList
    Remove copied ParseError and ErrorList and use base ParseError and ErrorList

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/moov-io/ofac
+
+require (
+	github.com/moov-io/base v0.3.0
+	github.com/rickar/cal v1.0.0 // indirect
+)
--- reader.go
+++ reader.go
@@ -1,15 +1,14 @@
 package ofac
 
 import (
-	"bytes"
 	"encoding/csv"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/moov-io/base"
 )
 
 const (
@@ -34,91 +33,23 @@ const (
 	speciallyDesignatedNationalCommentsFile = "sdn_comments.csv"
 )
 
-// Errors strings specific to parsing/reading an OFAC file
+// Error strings specific to parsing/reading an OFAC file
 var (
 	msgFileExtension = "%v is an invalid file type"
 	msgFileName      = "%v is an invalid file name"
 )
-
-// ParseError is returned for parsing reader errors.
-// The first line is 1.
-type ParseError struct {
-	Err error // The actual error
-}
-
-func (e ParseError) Error() string {
-	return fmt.Sprintf("%T %s", e.Err, e.Err)
-}
-
-// ErrorList represents an array of errors which is also an error itself.
-type ErrorList []error
-
-// Add appends err onto the ErrorList. Errors are kept in order.
-func (e *ErrorList) Add(err error) {
-	*e = append(*e, err)
-}
 
 // error returns a new ParseError based on err
 func (r *Reader) parseError(err error) error {
 	if err == nil {
 		return nil
 	}
-	if _, ok := err.(*ParseError); ok {
+	if _, ok := err.(*base.ParseError); ok {
 		return err
 	}
-	return &ParseError{
+	return &base.ParseError{
 		Err: err,
 	}
-}
-
-// Err returns the first error (or nil).
-func (e ErrorList) Err() error {
-	if e == nil || len(e) == 0 {
-		return nil
-	}
-	return e[0]
-}
-
-// Error implements the error interface
-func (e ErrorList) Error() string {
-	if len(e) == 0 {
-		return "<nil>"
-	}
-	var buf bytes.Buffer
-	e.Print(&buf)
-	return buf.String()
-}
-
-// Print formats the ErrorList into a string written to w.
-// If ErrorList contains multiple errors those after the first
-// are indented.
-func (e ErrorList) Print(w io.Writer) {
-	if w == nil || len(e) == 0 {
-		fmt.Fprintf(w, "<nil>")
-		return
-	}
-
-	fmt.Fprintf(w, "%s", e[0])
-	if len(e) > 1 {
-		fmt.Fprintf(w, "\n")
-	}
-
-	for i := 1; i < len(e); i++ {
-		fmt.Fprintf(w, "  %s", e[i])
-		if i < len(e)-1 { // don't add \n to last error
-			fmt.Fprintf(w, "\n")
-		}
-	}
-}
-
-// Empty no errors to return
-func (e ErrorList) Empty() bool {
-	return e == nil || len(e) == 0
-}
-
-// MarshalJSON marshals error list
-func (e ErrorList) MarshalJSON() ([]byte, error) {
-	return json.Marshal(e.Error())
 }
 
 // Reader reads records from a CSV file.
@@ -134,7 +65,7 @@ type Reader struct {
 	// SDNCommentsArray returns an array of OFAC Specially Designated National Comments
 	SDNCommentsArray []SDNComments `json:"sdnCommentsArray"`
 	// errors holds each error encountered when attempting to parse the file
-	errors ErrorList
+	errors base.ErrorList
 }
 
 // ToDo: Remove .del, .pip, .ff?
