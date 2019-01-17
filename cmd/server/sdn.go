64e0ec629b2a6a67d0eb5f965bcc9a4a89e98856
Adam Shannon <adamkshannon@gmail.com>
2019-01-17 13:00:14 -0600

    cmd/server: mock out SDN routes

--- cmd/server/sdn.go
+++ cmd/server/sdn.go
@@ -5,7 +5,11 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
+	moovhttp "github.com/moov-io/base/http"
+	"github.com/moov-io/ofac"
 
 	"github.com/go-kit/kit/log"
 	"github.com/gorilla/mux"
@@ -32,7 +36,31 @@ func getSDNAddresses(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
+
+		addresses := []*ofac.Address{
+			{
+				// 587,356,"Dai-Ichi Bldg. 6th Floor, 10-2 Nihombashi, 2-chome, Chuo-ku","Tokyo 103","Japan",-0-
+				EntityID:                    "587",
+				AddressID:                   "356",
+				Address:                     "Dai-Ichi Bldg. 6th Floor, 10-2 Nihombashi, 2-chome, Chuo-ku",
+				CityStateProvincePostalCode: "Tokyo 103",
+				Country:                     "Japan",
+			},
+			{
+				// 651,376,"Case Postale 236, 10 Bis Rue Du Vieux College 12-11","Geneva","Switzerland",-0-
+				EntityID:                    "651",
+				AddressID:                   "376",
+				Address:                     "Case Postale 236, 10 Bis Rue Du Vieux College 12-11",
+				CityStateProvincePostalCode: "Geneva",
+				Country:                     "Switzerland",
+			},
+		}
+		if err := json.NewEncoder(w).Encode(addresses); err != nil {
+			moovhttp.Problem(w, err) // TODO(adam): JSON errors should moovhttp.InternalError (wrapped, see auth's http.go)
+			return
+		}
 	}
 }
 
@@ -42,7 +70,29 @@ func getSDNAltNames(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
+
+		alts := []*ofac.AlternateIdentity{
+			{
+				// 815,641,"aka","GALAX INC.",-0-
+				EntityID:      "815",
+				AlternateID:   "641",
+				AlternateType: "aka",
+				AlternateName: "GALAX INC",
+			},
+			{
+				// 4359,3565,"fka","INDUSTRIA AVICOLA PALMASECA S.A.",-0-
+				EntityID:      "4359",
+				AlternateID:   "3565",
+				AlternateType: "fkaa",
+				AlternateName: "INDUSTRIA AVICOLA PALMASECA S.A.",
+			},
+		}
+		if err := json.NewEncoder(w).Encode(alts); err != nil {
+			moovhttp.Problem(w, err)
+			return
+		}
 	}
 }
 
@@ -52,6 +102,20 @@ func getSDN(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
+
+		sdn := &ofac.SDN{
+			EntityID: "3754",
+			SDNName:  "ABU MARZOOK, Mousa Mohammed",
+			SDNType:  "individual",
+			Program:  "SDGT] [SDT",
+			Title:    "Political Leader in Amman, Jordan and Damascus, Syria for HAMAS",
+			Remarks:  "DOB 09 Feb 1951; POB Gaza, Egypt; Passport 92/664 (Egypt); SSN 523-33-8386 (United States); Political Leader in Amman, Jordan and Damascus, Syria for HAMAS; a.k.a. 'ABU-'UMAR'.",
+		}
+		if err := json.NewEncoder(w).Encode(sdn); err != nil {
+			moovhttp.Problem(w, err)
+			return
+		}
 	}
 }
