ddb152061621d15a633783930f095adef17c6df9
Adam Shannon <adamkshannon@gmail.com>
2019-01-17 15:46:47 -0600

    cmd/server: mock out customer endpoints

--- cmd/server/customers.go
+++ cmd/server/customers.go
@@ -5,7 +5,11 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+
+	moovhttp "github.com/moov-io/base/http"
+	"github.com/moov-io/ofac"
 
 	"github.com/go-kit/kit/log"
 	"github.com/gorilla/mux"
@@ -14,7 +18,7 @@ import (
 // GET /customers/:id
 // - get customer information and matches
 
-// POST /customers/name/:name/watch
+// POST /customers/watch?name=...
 // - monitor customer by name, re-parse on each search
 
 // POST /customers/:id/watch
@@ -26,16 +30,30 @@ import (
 // DELETE /customers/:id/watch
 // - stop watching customer
 
-// DELETE /customers/name/:name/watch
+// DELETE /customers/watch?name=...
 // - stop watching customer name
+
+type Customer struct {
+	ID        string                    `json:"id"`
+	SDN       *ofac.SDN                 `json:"sdn"`
+	Addresses []*ofac.Address           `json:"addresses"`
+	Alts      []*ofac.AlternateIdentity `json:"alts"`
+	Comments  []*ofac.SDNComments       `json:"comments"`
+}
+
+type customerWatchResponse struct {
+	WatchID string `json:"watchId"`
+}
 
 func addCustomerRoutes(logger log.Logger, r *mux.Router) {
 	r.Methods("GET").Path("/customers/{id}").HandlerFunc(getCustomer(logger))
-	r.Methods("POST").Path("/customers/name/{name}/watch").HandlerFunc(addCustomerNameWatch(logger))
-	r.Methods("POST").Path("/customers/{id}/watch").HandlerFunc(addCustomerWatch(logger))
 	r.Methods("PUT").Path("/customers/{id}").HandlerFunc(updateCustomer(logger))
+
+	r.Methods("POST").Path("/customers/{id}/watch").HandlerFunc(addCustomerWatch(logger))
 	r.Methods("DELETE").Path("/customers/{id}/watch").HandlerFunc(removeCustomerWatch(logger))
-	r.Methods("DELETE").Path("/customers/name/{name}/watch").HandlerFunc(removeCustomerNameWatch(logger))
+
+	r.Methods("POST").Path("/customers/watch").HandlerFunc(addCustomerNameWatch(logger))
+	r.Methods("DELETE").Path("/customers/watch/{watchId}").HandlerFunc(removeCustomerNameWatch(logger))
 }
 
 func getCustomer(logger log.Logger) http.HandlerFunc {
@@ -44,7 +62,41 @@ func getCustomer(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
+
+		customer := Customer{
+			ID: "13ou1fohfkajfah", // "random"
+			SDN: &ofac.SDN{
+				EntityID: "2676",
+				SDNName:  "AL ZAWAHIRI, Dr. Ayman",
+				SDNType:  "individual",
+				Program:  "SDGT] [SDT",
+				Title:    "Operational and Military Leader of JIHAD GROUP",
+				Remarks:  "DOB 19 Jun 1951; POB Giza, Egypt; Passport 1084010 (Egypt); alt. Passport 19820215; Operational and Military Leader of JIHAD GROUP.",
+			},
+			Addresses: []*ofac.Address{
+				{
+					EntityID:                    "173",
+					AddressID:                   "129",
+					Address:                     "Ibex House, The Minories",
+					CityStateProvincePostalCode: "London EC3N 1DY",
+					Country:                     "United Kingdom",
+				},
+			},
+			Alts: []*ofac.AlternateIdentity{
+				{
+					EntityID:      "559",
+					AlternateID:   "481",
+					AlternateType: "aka",
+					AlternateName: "CIMEX",
+				},
+			},
+		}
+		if err := json.NewEncoder(w).Encode(customer); err != nil {
+			moovhttp.Problem(w, err) // TODO(adam): replace with wrapped moovhttp.InternalError
+			return
+		}
 	}
 }
 
@@ -54,7 +106,15 @@ func addCustomerNameWatch(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
+
+		// TODO: read ?name=... param
+
+		if err := json.NewEncoder(w).Encode(customerWatchResponse{"cust-name-watch"}); err != nil {
+			moovhttp.Problem(w, err)
+			return
+		}
 	}
 }
 
@@ -64,7 +124,13 @@ func addCustomerWatch(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
+
+		if err := json.NewEncoder(w).Encode(customerWatchResponse{"cust-watch"}); err != nil {
+			moovhttp.Problem(w, err)
+			return
+		}
 	}
 }
 
@@ -74,7 +140,7 @@ func updateCustomer(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusOK) // TODO
 	}
 }
 
@@ -84,7 +150,7 @@ func removeCustomerWatch(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusOK) // TODO
 	}
 }
 
@@ -94,6 +160,6 @@ func removeCustomerNameWatch(logger log.Logger) http.HandlerFunc {
 		if err != nil {
 			return
 		}
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusOK) // TODO
 	}
 }
---
+++ cmd/server/customers_test.go
@@ -0,0 +1,123 @@
+// Copyright 2018 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCustomer_get(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/customers/foo", nil)
+	req.Header.Set("x-user-id", "test")
+
+	getCustomer(nil)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+
+	var cust Customer
+	if err := json.NewDecoder(w.Body).Decode(&cust); err != nil {
+		t.Fatal(err)
+	}
+	if cust.ID == "" {
+		t.Fatalf("empty ofac.Customer: %#v", cust)
+	}
+	if cust.SDN == nil {
+		t.Fatal("missing cust.SDN")
+	}
+	if len(cust.Addresses) != 1 {
+		t.Errorf("cust.Addresses: %#v", cust.Addresses)
+	}
+	if len(cust.Alts) != 1 {
+		t.Errorf("cust.Alts: %#v", cust.Alts)
+	}
+}
+
+func TestCustomer_addWatch(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/customers/foo/watch", nil)
+	req.Header.Set("x-user-id", "test")
+
+	addCustomerWatch(nil)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+
+	var watch customerWatchResponse
+	if err := json.NewDecoder(w.Body).Decode(&watch); err != nil {
+		t.Fatal(err)
+	}
+	if watch.WatchID == "" {
+		t.Error("empty watch.WatchID")
+	}
+}
+
+func TestCustomer_addNameWatch(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/customers/watch?name=foo", nil)
+	req.Header.Set("x-user-id", "test")
+
+	addCustomerWatch(nil)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+
+	var watch customerWatchResponse
+	if err := json.NewDecoder(w.Body).Decode(&watch); err != nil {
+		t.Fatal(err)
+	}
+	if watch.WatchID == "" {
+		t.Error("empty watch.WatchID")
+	}
+}
+
+func TestCustomer_update(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("PUT", "/customers/foo", nil)
+	req.Header.Set("x-user-id", "test")
+
+	updateCustomer(nil)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+}
+
+func TestCustomer_removeWatch(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("DELETE", "/customers/foo/watch", nil)
+	req.Header.Set("x-user-id", "test")
+
+	removeCustomerWatch(nil)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+}
+
+func TestCustomer_removeNameWatch(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("DELETE", "/customers/watch/foo", nil)
+	req.Header.Set("x-user-id", "test")
+
+	removeCustomerNameWatch(nil)(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+}
