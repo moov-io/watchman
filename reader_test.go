0cdcc9cc09b9fc1007afee3b6779d582c7ff1d4e
Brooke E Kline Jr <brooke.kline@moov.io>
2019-01-17 16:32:37 -0500

    Increase reader.go test coverage
    Increase reader.go test coverage

--- reader_test.go
+++ reader_test.go
@@ -1,11 +1,8 @@
-// Copyright 2018 The Moov Authors
-// Use of this source code is governed by an Apache License
-// license that can be found in the LICENSE file.
-
 package ofac
 
 import (
 	"encoding/json"
+	"os"
 	"strings"
 	"testing"
 )
@@ -108,5 +105,126 @@ func TestSDNCSVFileJSON(t *testing.T) {
 	if _, err := json.Marshal(r.SDNArray); err != nil {
 		t.Errorf("%T: %s", err, err)
 	}
+}
 
+// TestInvalidAddressCSVFile validates validates an invalid Address CSV File returns an error
+func TestInvalidAddressCSVFile(t *testing.T) {
+	r := Reader{}
+
+	r.FileName = "invalid/add.csv"
+	if err := r.Read(); err != nil {
+		if e, ok := err.(*os.PathError); ok {
+			if !strings.Contains(e.Error(), "The system cannot find the path specified") {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestInvalidAlternateIDCSVFile validates validates an invalid Alternate ID CSV File returns an error
+func TestInvalidAlternateIDCSVFile(t *testing.T) {
+	r := Reader{}
+
+	r.FileName = "invalid/alt.csv"
+	if err := r.Read(); err != nil {
+		if e, ok := err.(*os.PathError); ok {
+			if !strings.Contains(e.Error(), "The system cannot find the path specified") {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+
+	}
+}
+
+// TestInvalidSDNCSVFile validates an invalid SDN CSV File returns an error
+func TestInvalidSDNCSVFile(t *testing.T) {
+	r := Reader{}
+	r.FileName = "invalid/sdn.csv"
+	if err := r.Read(); err != nil {
+		if e, ok := err.(*os.PathError); ok {
+			if !strings.Contains(e.Error(), "The system cannot find the path specified") {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestInvalidSDNCommentsCSVFile validates an invalid SDNComments CSV File returns an error
+func TestInvalidSDNCommentsCSVFile(t *testing.T) {
+	r := Reader{}
+	r.FileName = "invalid/sdn_comments.csv"
+	if err := r.Read(); err != nil {
+		if e, ok := err.(*os.PathError); ok {
+			if !strings.Contains(e.Error(), "The system cannot find the path specified") {
+				t.Errorf("%T: %s", err, err)
+			}
+		} else {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestAddressCSVInvalidLineRead validates an error is returned when reading an OFAC Specially Designated National
+// Address CSV File with an invalid line
+func TestAddressCSVInvalidLineRead(t *testing.T) {
+	r := Reader{}
+	r.FileName = "test/testdata/invalidFiles/add.csv"
+	if err := r.Read(); err != nil {
+		if !strings.Contains(err.Error(), "wrong number of fields") {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestAlternateIDCSVInvalidLineRead validates an error is returned when reading an OFAC Specially Designated National
+// Alternate ID CSV File with an invalid line
+func TestAlternateIDCSVInvalidLineRead(t *testing.T) {
+	r := Reader{}
+	r.FileName = "test/testdata/invalidFiles/alt.csv"
+	if err := r.Read(); err != nil {
+		if !strings.Contains(err.Error(), "wrong number of fields") {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestSDNCSVInvalidLineRead validates an error is returned when reading an OFAC Specially Designated National
+// CSV File with an invalid line
+func TestSDNCSVInvalidLineRead(t *testing.T) {
+	r := Reader{}
+	r.FileName = "test/testdata/invalidFiles/sdn.csv"
+	if err := r.Read(); err != nil {
+		if !strings.Contains(err.Error(), "wrong number of fields") {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestSDNCommentsCSVInvalidLineRead validates an error is returned when reading an OFAC Specially Designated National
+// Comments CSV File with an invalid line
+func TestSDNCommentsCSVInvalidLineRead(t *testing.T) {
+	r := Reader{}
+	r.FileName = "test/testdata/invalidFiles/sdn_comments.csv"
+	if err := r.Read(); err != nil {
+		if !strings.Contains(err.Error(), "wrong number of fields") {
+			t.Errorf("%T: %s", err, err)
+		}
+	}
+}
+
+// TestParseError validates parseError
+func TestParseError(t *testing.T) {
+	r := Reader{}
+	r.FileName = "test/testdata/sdn_comments.csv"
+	err := r.Read()
+
+	if e := r.parseError(err); e != nil {
+		t.Errorf("%T: %s", e, e)
+	}
 }
