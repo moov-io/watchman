d5ad41f88d74ab33ed0dac2b7d5d79893d1d2deb
Adam Shannon <adamkshannon@gmail.com>
2019-02-01 13:00:38 -0600

    pkg/strcmp: remove JaroWinkler and un-export Hamming func

--- pkg/strcmp/strcmp.go
+++ pkg/strcmp/strcmp.go
@@ -15,23 +15,12 @@ import (
 	"github.com/xrash/smetrics"
 )
 
-// JaroWinkler is a more accurate version of the Jaro algorithm. It works by boosting the
-// score of exact matches at the beginning of the strings. By doing this, Winkler says that
-// typos are less common to happen at the beginning.
-//
-// For this to happen, it introduces two more parameters: the boostThreshold and the prefixSize.
-// These are commonly set to 0.7 and 4, respectively.
-//
-// From: https://godoc.org/github.com/xrash/smetrics
-func JaroWinkler(a, b string) float64 {
-	if a == "" || b == "" {
-		return 0.00
-	}
-	return smetrics.JaroWinkler(a, b, 0.7, 4)
-}
-
 // Levenshtein is the "edit distance" between two strings. This is the count of operations
 // (insert, delete, replace) needed for two strings to be equal.
+//
+// The returend value represents a score between 0-1 for how similar the strings are.
+//  Levenshtein("abc", "abc") == 1
+//  Levenshtein("abb", "abb") == 0.66
 func Levenshtein(a, b string) float64 {
 	if a == "" || b == "" {
 		return 0.00
@@ -41,14 +30,17 @@ func Levenshtein(a, b string) float64 {
 	if n := utf8.RuneCountInString(b); n > length {
 		length = n // set length to larger value of the two strings
 	}
-	ed := float64(smetrics.WagnerFischer(a, b, 1, 1, 2))
 
+	ed := float64(smetrics.WagnerFischer(a, b, 1, 1, 2))
+	if ed == 0 {
+		return 1.0 // strings are equal
+	}
 	score := ed / float64(length)
 	if score > 1.00 {
 		// If more edits are required than the string's length a and b aren't equal.
 		return 0.00
 	}
-	return 1 - score
+	return score
 }
 
 // Soundex is a phonetic algorithm that considers how the words sound in english.
@@ -59,11 +51,11 @@ func Soundex(a, b string) float64 {
 	if a == "" || b == "" {
 		return 0.00
 	}
-	return Hamming(smetrics.Soundex(a), smetrics.Soundex(b))
+	return hamming(smetrics.Soundex(a), smetrics.Soundex(b))
 }
 
-// Hamming distance is the minimum number of substitutions required to change one string into the other.
-func Hamming(a, b string) float64 {
+// hamming distance is the minimum number of substitutions required to change one string into the other.
+func hamming(a, b string) float64 {
 	if a == "" || b == "" {
 		return 0.00
 	}
