2bde8d3ccb8f608f10bc17f0dce610b487262c9e
Adam Shannon <adamkshannon@gmail.com>
2019-02-01 18:24:16 -0600

    cmd/server: ensure webhookHTTPClient never follows redirects

--- cmd/server/webhook_test.go
+++ cmd/server/webhook_test.go
@@ -5,10 +5,56 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestWebhooks_validate(t *testing.T) {
+var (
+	redirect = func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Location", "https://example.com")
+		w.WriteHeader(http.StatusMovedPermanently)
+		w.Write([]byte("didn't redirect"))
+	}
+)
+
+// TestWebhook_retry ensures the webhookHTTPClient never follows a redirect.
+// This is done to prevent infinite (or costly) redirect cycles which can degrade performance.
+func TestWebhook_retry(t *testing.T) {
+	if testing.Short() {
+		return
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(redirect))
+	defer server.Close()
+
+	// normal client, ensure redirect is followed
+	resp, err := server.Client().Get(server.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ensure we landed on example.com
+	bs, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if !bytes.Contains(bs, []byte("iana.org")) {
+		t.Errorf("resp.Body=%s", string(bs))
+	}
+
+	// Now ensure our webhookHTTPClient doesn't follow the redirect
+	resp, err = webhookHTTPClient.Get(server.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bs, _ = ioutil.ReadAll(resp.Body)
+	if !bytes.Contains(bs, []byte("didn't redirect")) {
+		t.Errorf("resp.Body=%s", string(bs))
+	}
+}
+
+func TestWebhook_validate(t *testing.T) {
 	out, err := validateWebhook("")
 	if err == nil {
 		t.Error("expected error")
