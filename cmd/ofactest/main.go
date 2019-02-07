49169438e32fb0f6fa90b1fb658c81fbe7b3cf4d
Adam Shannon <adamkshannon@gmail.com>
2019-02-06 21:26:48 -0600

    cmd/ofactest: get latest download as a test

--- cmd/ofactest/main.go
+++ cmd/ofactest/main.go
@@ -6,6 +6,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -14,6 +15,8 @@ import (
 
 	"github.com/moov-io/ofac"
 	moov "github.com/moov-io/ofac/client"
+
+	"github.com/antihax/optional"
 )
 
 var (
@@ -35,7 +38,6 @@ func main() {
 		conf.BasePath = v
 	}
 	conf.UserAgent = fmt.Sprintf("moov/ofactest:%s", ofac.Version)
-
 	conf.HTTPClient = &http.Client{
 		Timeout: 10 * time.Second,
 		Transport: &http.Transport{
@@ -43,10 +45,12 @@ func main() {
 		},
 	}
 
+	log.Printf("[INFO] using %s for address", conf.BasePath)
+
 	// Read OAuth token and set on conf
 	accessToken := GetOAuthToken(OAuthTokenStorageFilepath)
 	if accessToken == "" {
-		log.Fatalf("[FAILURE] no OAuth token found, run moov/apitest locally")
+		log.Fatal("[FAILURE] no OAuth token found, run moov/apitest locally")
 	} else {
 		conf.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 	}
@@ -55,15 +59,17 @@ func main() {
 	api, ctx := moov.NewAPIClient(conf), context.TODO()
 
 	// Ping OFAC
-	resp, err := api.OFACApi.Ping(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		log.Printf("[FAILURE] ping error (stats code: %d): %v", resp.StatusCode, err)
+	if err := ping(ctx, api); err != nil {
+		log.Fatal("[FAILURE] ping OFAC")
 	} else {
-		log.Println("[SUCCESS] ping OFAC")
+		log.Println("[SUCCESS] ping")
+	}
+
+	// Check downloads
+	if when, err := latestDownload(ctx, api); err != nil || when.IsZero() {
+		log.Fatalf("[FAILURE] downloads: %v", err)
+	} else {
+		log.Printf("[SUCCESS] last download was: %v ago", time.Since(when).Truncate(1*time.Second))
 	}
 
 	// Search queries
@@ -72,4 +78,33 @@ func main() {
 	} else {
 		log.Println("[SUCCESS] search queries passed")
 	}
+}
+
+func ping(ctx context.Context, api *moov.APIClient) error {
+	resp, err := api.OFACApi.Ping(ctx)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf("ping error (stats code: %d): %v", resp.StatusCode, err)
+	}
+	return nil
+}
+
+func latestDownload(ctx context.Context, api *moov.APIClient) (time.Time, error) {
+	downloads, resp, err := api.OFACApi.GetLatestDownloads(ctx, &moov.GetLatestDownloadsOpts{
+		Limit: optional.NewInt32(1),
+	})
+	if err != nil {
+		return time.Time{}, err
+	}
+	resp.Body.Close()
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return time.Time{}, fmt.Errorf("download error (stats code: %d): %v", resp.StatusCode, err)
+	}
+	if len(downloads) == 0 {
+		return time.Time{}, errors.New("empty downloads response")
+	}
+	return downloads[0].Timestamp, nil
 }
