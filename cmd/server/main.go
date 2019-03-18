372b626c7ac6186f6c944bbf7f102a2fd20abdce
Adam Shannon <adamkshannon@gmail.com>
2019-03-18 11:21:48 -0500

    cmd/server: support -log.format=json

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -29,13 +30,20 @@ var (
 	httpAddr  = flag.String("http.addr", bind.HTTP("ofac"), "HTTP listen address")
 	adminAddr = flag.String("admin.addr", bind.Admin("ofac"), "Admin HTTP listen address")
 
+	flagLogFormat = flag.String("log.format", "", "Format for log lines (Options: json, plain")
+
 	ofacDataRefreshInterval = 12 * time.Hour
 )
 
 func main() {
 	flag.Parse()
 
-	logger := log.NewLogfmtLogger(os.Stderr)
+	var logger log.Logger
+	if strings.ToLower(*flagLogFormat) == "json" {
+		logger = log.NewJSONLogger(os.Stderr)
+	} else {
+		logger = log.NewLogfmtLogger(os.Stderr)
+	}
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
 	logger = log.With(logger, "caller", log.DefaultCaller)
 
