549bfd56533750de23a8ef0d5f0982d1ee0e4924
Adam Shannon <adamkshannon@gmail.com>
2019-04-30 19:44:18 -0500

    cmd/server: remove moov/base.ID() values from Prometheus metric labels

--- cmd/server/http.go
+++ cmd/server/http.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -32,14 +33,21 @@ func wrapResponseWriter(logger log.Logger, w http.ResponseWriter, r *http.Reques
 	return moovhttp.Wrap(logger, routeHistogram.With("route", route), w, r)
 }
 
+var baseIdRegex = regexp.MustCompile(`([a-f0-9]{40})`)
+
 // cleanMetricsPath takes a URL path and formats it for Prometheus metrics
-// This method replaces /'s with -'s and clean out OFAC ID's (which are numeric)
+//
+// This method replaces /'s with -'s and clean out OFAC ID's (which are numeric).
+// This method also strips out moov/base.ID() values from URL path slugs.
 func cleanMetricsPath(path string) string {
 	parts := strings.Split(path, "/")
 	var out []string
 	for i := range parts {
 		if n, _ := strconv.Atoi(parts[i]); n > 0 || parts[i] == "" {
-			continue
+			continue // numeric ID
+		}
+		if baseIdRegex.MatchString(parts[i]) {
+			continue // assume it's a moov/base.ID() value
 		}
 		out = append(out, parts[i])
 	}
--- cmd/server/http_test.go
+++ cmd/server/http_test.go
@@ -15,4 +15,11 @@ func TestHTTP__cleanMetricsPath(t *testing.T) {
 	if v := cleanMetricsPath("/v1/ofac/ping"); v != "v1-ofac-ping" {
 		t.Errorf("got %q", v)
 	}
+	if v := cleanMetricsPath("/v1/ofac/customers/19636f90bc95779e2488b0f7a45c4b68958a2ddd"); v != "v1-ofac-customers" {
+		t.Errorf("got %q", v)
+	}
+	// A value which looks like moov/base.ID, but is off by one character (last letter)
+	if v := cleanMetricsPath("/v1/ofac/customers/19636f90bc95779e2488b0f7a45c4b68958a2ddz"); v != "v1-ofac-customers-19636f90bc95779e2488b0f7a45c4b68958a2ddz" {
+		t.Errorf("got %q", v)
+	}
 }
