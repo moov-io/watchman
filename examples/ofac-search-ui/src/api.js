b13fc77138a9fcb0fcfa739cd93f174d8667cfc5
Franklin Henderson <fhwrdh@fhwrdh.net>
2019-09-27 08:13:47 -0700

    setup proxy for devmode; update client api calls

--- examples/ofac-search-ui/src/api.js
+++ examples/ofac-search-ui/src/api.js
@@ -1,14 +1,14 @@
 export const search = async qs => {
-  const r = await fetch(`/search?${qs}`);
+  const r = await fetch(`/api/search?${qs}`);
   return await r.json();
 };
 
 export const getSDNAlts = async sdnId => {
-  const r = await fetch(`/sdn/${sdnId}/alts`);
+  const r = await fetch(`/api/sdn/${sdnId}/alts`);
   return await r.json();
 };
 
 export const getSDNAddresses = async sdnId => {
-  const r = await fetch(`/sdn/${sdnId}/addresses`);
+  const r = await fetch(`/api/sdn/${sdnId}/addresses`);
   return await r.json();
 };
