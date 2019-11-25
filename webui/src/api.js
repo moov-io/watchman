a347f0fc202189024e68088fc6f490ff578c1432
Adam Shannon <adamkshannon@gmail.com>
2019-11-25 11:53:19 -0600

    webui: prepend /ofac/ prefix

--- webui/src/api.js
+++ webui/src/api.js
@@ -10,7 +10,7 @@ const pathname = new URL(document.baseURI).origin + "/";
 
 export const search = async qs => apiGet(pathname + `/search?${qs}`);
 
-export const getSDNAlts = async sdnId => (await fetch(pathname + `/sdn/${sdnId}/alts`)).json();
-export const getSDNAddresses = async sdnId => (await fetch(pathname + `/sdn/${sdnId}/addresses`)).json();
+export const getSDNAlts = async sdnId => (await fetch(pathname + `/ofac/sdn/${sdnId}/alts`)).json();
+export const getSDNAddresses = async sdnId => (await fetch(pathname + `/ofac/sdn/${sdnId}/addresses`)).json();
 export const getSDNTypes = async qs => (await fetch(pathname + `/ui/values/sdnType`)).json();
 export const getOFACPrograms = async qs => (await fetch(pathname + `/ui/values/ofacProgram`)).json();
