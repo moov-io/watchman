d05c72266796104243ec356fc984f8a797da8718
Adam Shannon <adamkshannon@gmail.com>
2019-12-06 11:44:10 -0600

    build: linter cleanup

--- cmd/server/stopwords.go
+++ cmd/server/stopwords.go
@@ -22,7 +22,7 @@ func removeStopwords(in string, lang whatlanggo.Lang) string {
 	return strings.TrimSpace(stopwords.CleanString(strings.ToLower(in), lang.Iso6391(), false))
 }
 
-// detectLanguage will return a guess as to the appropiate language a given SDN's name
+// detectLanguage will return a guess as to the appropriate language a given SDN's name
 // is written in. The addresses must be linked to the SDN whose name is detected.
 func detectLanguage(in string, addrs []*ofac.Address) whatlanggo.Lang {
 	info := whatlanggo.Detect(in)
@@ -36,7 +36,7 @@ func detectLanguage(in string, addrs []*ofac.Address) whatlanggo.Lang {
 		return whatlanggo.Eng
 	}
 
-	// Return the countries primary langauge associated to the primary address for this SDN.
+	// Return the countries primary language associated to the primary address for this SDN.
 	//
 	// TODO(adam): Should we do this only if there's one address? If there are multiple should we
 	// fallback to English or a mixed set?
@@ -48,14 +48,14 @@ func detectLanguage(in string, addrs []*ofac.Address) whatlanggo.Lang {
 	// If the language is spoken in the country and we're somewhat confident in the original detection
 	// then return that language.
 	if info.Confidence > minConfidence {
-		for key, _ := range country.Languages {
+		for key := range country.Languages {
 			if strings.EqualFold(key, info.Lang.Iso6393()) {
 				return info.Lang
 			}
 		}
 	}
 	if len(country.Languages) == 1 {
-		for key, _ := range country.Languages {
+		for key := range country.Languages {
 			return whatlanggo.CodeToLang(key)
 		}
 	}
