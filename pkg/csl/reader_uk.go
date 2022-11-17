3a9c13d977c532cdb71e5d5117b772c7ec505115
diginicho <nicholas.rivera@digicert.com>
2022-11-17 16:58:44 -0700

    adds a reader to start parsing uk data

---
+++ pkg/csl/reader_uk.go
@@ -0,0 +1,131 @@
+package csl
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func ReadUKFile(path string) ([]*UKCSLRecord, UKCSL, error) {
+	fd, err := os.Open(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer fd.Close()
+
+	rows, rowsMap, err := ParseUK(fd)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return rows, rowsMap, nil
+}
+
+func ParseUK(r io.Reader) ([]*UKCSLRecord, UKCSL, error) {
+	reader := csv.NewReader(r)
+	reader.FieldsPerRecord = 36
+
+	report := make(UKCSL)
+	// read and ignore first two rows
+	for i := 0; i <= 1; i++ {
+		reader.Read()
+	}
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			// reached the last line
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			// malformed row
+			if errors.Is(err, csv.ErrFieldCount) ||
+				errors.Is(err, csv.ErrBareQuote) ||
+				errors.Is(err, csv.ErrQuote) {
+				continue
+			}
+			return nil, nil, err
+		}
+
+		if len(record) <= 1 {
+			fmt.Println("record is <= 1", record)
+			continue // skip empty records
+		}
+
+		// merge rows at this point
+		// for each record we need to add that to the map
+		groupID, err := strconv.Atoi(record[GroupdIdx])
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// check if entry does not exist
+		if val, ok := report[groupID]; !ok {
+			// creates the initial record
+			row := new(UKCSLRecord)
+			unmarshalUKRecord(record, row)
+
+			report[groupID] = row
+		} else {
+			// we found an entry in the map and need to append
+			unmarshalUKRecord(record, val)
+		}
+
+	}
+	var totalReport []*UKCSLRecord
+	for _, row := range report {
+		totalReport = append(totalReport, row)
+	}
+	return totalReport, report, nil
+}
+
+func unmarshalUKRecord(csvRecord []string, ukCSLRecord *UKCSLRecord) {
+	if csvRecord[UKNameIdx] != "" {
+		ukCSLRecord.Names = append(ukCSLRecord.Names, csvRecord[UKNameIdx])
+	}
+	if csvRecord[UKTitleIdx] != "" {
+		ukCSLRecord.Titles = append(ukCSLRecord.Titles, csvRecord[UKTitleIdx])
+	}
+	if csvRecord[DOBhIdx] != "" {
+		ukCSLRecord.DatesOfBirth = append(ukCSLRecord.DatesOfBirth, csvRecord[DOBhIdx])
+	}
+	if csvRecord[TownOfBirthIdx] != "" {
+		ukCSLRecord.TownsOfBirth = append(ukCSLRecord.TownsOfBirth, csvRecord[TownOfBirthIdx])
+	}
+	if csvRecord[CountryOfBirthIdx] != "" {
+		ukCSLRecord.CountriesOfBirth = append(ukCSLRecord.CountriesOfBirth, csvRecord[CountryOfBirthIdx])
+	}
+	if csvRecord[UKNationalitiesIdx] != "" {
+		ukCSLRecord.Nationalities = append(ukCSLRecord.Nationalities, csvRecord[UKNationalitiesIdx])
+	}
+	if csvRecord[AddressOneIdx] != "" {
+		ukCSLRecord.Addresses = append(ukCSLRecord.Addresses, csvRecord[AddressOneIdx])
+	}
+	if csvRecord[AddressTwoIdx] != "" {
+		ukCSLRecord.AddressesTwo = append(ukCSLRecord.AddressesTwo, csvRecord[AddressTwoIdx])
+	}
+	if csvRecord[AddressThreeIdx] != "" {
+		ukCSLRecord.AddressesThree = append(ukCSLRecord.AddressesThree, csvRecord[AddressThreeIdx])
+	}
+	if csvRecord[AddressFourIdx] != "" {
+		ukCSLRecord.AddressesFour = append(ukCSLRecord.AddressesFour, csvRecord[AddressFourIdx])
+	}
+	if csvRecord[AddressFiveIdx] != "" {
+		ukCSLRecord.AddressesFive = append(ukCSLRecord.AddressesFive, csvRecord[AddressFiveIdx])
+	}
+	if csvRecord[AddressSixIdx] != "" {
+		ukCSLRecord.AddressesSix = append(ukCSLRecord.AddressesSix, csvRecord[AddressSixIdx])
+	}
+	if csvRecord[PostalCodeIdx] != "" {
+		ukCSLRecord.PostalCodes = append(ukCSLRecord.PostalCodes, csvRecord[PostalCodeIdx])
+	}
+	if csvRecord[CountryIdx] != "" {
+		ukCSLRecord.Countries = append(ukCSLRecord.Countries, csvRecord[CountryIdx])
+	}
+	if csvRecord[OtherInfoIdx] != "" {
+		ukCSLRecord.OtherInfos = append(ukCSLRecord.OtherInfos, csvRecord[OtherInfoIdx])
+	}
+}
--- pkg/csl/uk_csl.go
+++ pkg/csl/uk_csl.go
@@ -4,51 +4,52 @@
 
 package csl
 
+type UKCSL map[int]*UKCSLRecord
+
+// Indices we care about for UK - CSL row data
 const (
-	UKNameIdx = 0
-	UKTitleIdx = 6
-	DOBhIdx = 10
-	TownOfBirthIdx = 11
-	CountryOfBirthIdx = 12
+	UKNameIdx          = 0
+	UKTitleIdx         = 6
+	DOBhIdx            = 10
+	TownOfBirthIdx     = 11
+	CountryOfBirthIdx  = 12
 	UKNationalitiesIdx = 13
-	AddressOneIdx = 19 // Address Line 1
-	AddressTwoIdx = 20 // Address Line 2
-	AddressThreeIdx = 21 // 
-	AddressFourIdx = 22 
-	AddressFiveIdx = 23
-	AddressSixIdx = 24
-	PostalCodeIdx = 25
-	CountryIdx = 26
-	OtherInfoIdx = 27
-	GroupTypeidx = 28
-	ListedDateIdx = 32
-	UKSancListDateIdx = 33
-	LastUpdatedIdx = 34
-	GroupdIdx = 35
+	AddressOneIdx      = 19
+	AddressTwoIdx      = 20
+	AddressThreeIdx    = 21
+	AddressFourIdx     = 22
+	AddressFiveIdx     = 23
+	AddressSixIdx      = 24
+	PostalCodeIdx      = 25
+	CountryIdx         = 26
+	OtherInfoIdx       = 27
+	GroupTypeidx       = 28
+	ListedDateIdx      = 32
+	UKSancListDateIdx  = 33
+	LastUpdatedIdx     = 34
+	GroupdIdx          = 35
 )
 
 // UK is the UK Consolidated List of Financial Sanctions Targets
 type UKCSLRecord struct {
-	Names []string `json:"names"`
-	Titles []string `json:"titles"`
-	DatesOfBirth []string `json:"datesOfBirth"`
-	TownsOfBirth []string `json:"townsOfBirth"`
-	CountriesOfBirth []string `json:"countriesOfBirth"`
-	Nationalities []string `json:"nationalities"`
-	Addresses []string `json:"addresses"` // Address Line 1
-	AddressesTwo []string `json:"addressesTwo"` // Address Line 2
-	AddressesThree []string `json:"addressesThree"` // Address Line 3
-	AddressesFour []string `json:"addressesFour"` // Address Line 4
-	AddressesFive []string `json:"addressesFive"` // Address Line 5
-	AddressesSix []string `json:"addressesSix"` // Address Line 6
-	PostalCodes []string `json:"postalCodes"`
-	Countries []string `json:"countries"`
-	OtherInfos []string `json:"otherInfo"`
-	GroupTypes []string `json:"groupType"`
-	ListedDates []string `json:"listedDate"`
+	Names             []string `json:"names"`
+	Titles            []string `json:"titles"`
+	DatesOfBirth      []string `json:"datesOfBirth"`
+	TownsOfBirth      []string `json:"townsOfBirth"`
+	CountriesOfBirth  []string `json:"countriesOfBirth"`
+	Nationalities     []string `json:"nationalities"`
+	Addresses         []string `json:"addresses"`
+	AddressesTwo      []string `json:"addressesTwo"`
+	AddressesThree    []string `json:"addressesThree"`
+	AddressesFour     []string `json:"addressesFour"`
+	AddressesFive     []string `json:"addressesFive"`
+	AddressesSix      []string `json:"addressesSix"`
+	PostalCodes       []string `json:"postalCodes"`
+	Countries         []string `json:"countries"`
+	OtherInfos        []string `json:"otherInfo"`
+	GroupTypes        []string `json:"groupType"`
+	ListedDates       []string `json:"listedDate"`
 	SanctionListDates []string `json:"sanctionListDate"`
-	LastUpdates []string `json:"lastUpdated"`
-	GroupIDs []int `json:"groups"`
+	LastUpdates       []string `json:"lastUpdated"`
+	GroupID           int      `json:"groupId"`
 }
-
-
