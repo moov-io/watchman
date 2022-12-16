3329642ae4812cb0038144a94fa9b9b37009b08c
Adam Shannon <adamkshannon@gmail.com>
2022-12-16 09:13:57 -0600

    refactor: cleanup append

--- cmd/server/search_handlers.go
+++ cmd/server/search_handlers.go
@@ -351,27 +351,8 @@ var (
 		},
 	}
 
-	allGatherings = concateMultipleSlices([][]searchGather{baseGatherings, cslGatherings, euGatherings, ukGatherings})
+	allGatherings = append(append(append(baseGatherings, cslGatherings...), euGatherings...), ukGatherings...)
 )
-
-// this function taken from here https://freshman.tech/snippets/go/concatenate-slices/
-// and modified to be specific to searchGather slices
-func concateMultipleSlices(slices [][]searchGather) []searchGather {
-	var totalLen int
-	for _, s := range slices {
-		totalLen += len(s)
-	}
-
-	result := make([]searchGather, totalLen)
-
-	var i int
-
-	for _, s := range slices {
-		i += copy(result[i:], s)
-	}
-
-	return result
-}
 
 func buildFullSearchResponse(searcher *searcher, filters filterRequest, limit int, minMatch float64, name string) *searchResponse {
 	return buildFullSearchResponseWith(searcher, allGatherings, filters, limit, minMatch, name)
