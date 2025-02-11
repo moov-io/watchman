f93f712205f1d3d47c9f59b1d6e9eb13ee1edb1e
Adam Shannon <adamkshannon@gmail.com>
2025-02-11 14:46:10 -0600

    search: benchmark Similarity with debug and without
    It's known that debugging is expensive (in terms of memory), but start
    to track the difference.
    
    BenchmarkDebugSimilarity/individuals-16         	   29882	     39286 ns/op	    1776 B/op	     135 allocs/op
    BenchmarkDebugSimilarity/individuals-debug-16   	   18225	     67002 ns/op	    9098 B/op	     261 allocs/op
    BenchmarkDebugSimilarity/businesses-16          	   72868	     17118 ns/op	    1228 B/op	      72 allocs/op
    BenchmarkDebugSimilarity/businesses-debug-16    	   31266	     38642 ns/op	    8297 B/op	     178 allocs/op
    BenchmarkDebugSimilarity/vessels-16             	  391942	      2948 ns/op	     184 B/op	      16 allocs/op
    BenchmarkDebugSimilarity/vessels-debug-16       	   64852	     18577 ns/op	    7055 B/op	     105 allocs/op
    BenchmarkDebugSimilarity/aircraft-16            	    6450	    169607 ns/op	    7136 B/op	     496 allocs/op
    BenchmarkDebugSimilarity/aircraft-debug-16      	    6866	    225193 ns/op	   21641 B/op	     702 allocs/op

---
+++ pkg/search/similarity_bench_test.go
@@ -0,0 +1,75 @@
+package search_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/moov-io/watchman/internal/ofactest"
+	"github.com/moov-io/watchman/pkg/search"
+)
+
+func BenchmarkDebugSimilarity(b *testing.B) {
+	// Load OFAC records once, before any subtest starts
+	ofactest.FindEntity(b, "29702")
+
+	bench := func(b *testing.B, name string, entities []search.Entity[search.Value], debug bool) {
+		b.Helper()
+
+		if debug {
+			name += "-debug"
+		}
+		b.ResetTimer()
+
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				query := i % (len(entities) - 1)
+				index := (i + 1) % (len(entities) - 1)
+
+				if debug {
+					var buf bytes.Buffer
+					search.DebugSimilarity(&buf, entities[query], entities[index])
+				} else {
+					search.Similarity(entities[query], entities[index])
+				}
+			}
+		})
+	}
+
+	// individuals
+	individuals := []search.Entity[search.Value]{
+		ofactest.FindEntity(b, "15102"),
+		ofactest.FindEntity(b, "29702"),
+		ofactest.FindEntity(b, "48603"),
+	}
+	bench(b, "individuals", individuals, false)
+	bench(b, "individuals", individuals, true)
+
+	// businesses
+	businesses := []search.Entity[search.Value]{
+		ofactest.FindEntity(b, "12685"),
+		ofactest.FindEntity(b, "28603"),
+		ofactest.FindEntity(b, "33151"),
+		ofactest.FindEntity(b, "44525"),
+		ofactest.FindEntity(b, "50544"),
+	}
+	bench(b, "businesses", businesses, false)
+	bench(b, "businesses", businesses, true)
+
+	// vessel
+	vessels := []search.Entity[search.Value]{
+		ofactest.FindEntity(b, "47371"),
+		ofactest.FindEntity(b, "50972"),
+		ofactest.FindEntity(b, "52327"),
+	}
+	bench(b, "vessels", vessels, false)
+	bench(b, "vessels", vessels, true)
+
+	// aircraft
+	aircraft := []search.Entity[search.Value]{
+		ofactest.FindEntity(b, "11195"),
+		ofactest.FindEntity(b, "19709"),
+		ofactest.FindEntity(b, "48727"),
+	}
+	bench(b, "aircraft", aircraft, false)
+	bench(b, "aircraft", aircraft, true)
+}
