d4bf4b6b804778e43b3aa93beffcf6f744a69bd8
Adam Shannon <adamkshannon@gmail.com>
2026-06-03 08:42:05 -0500

    fix: init libpostal before accepting requests (#758)
    Fixes: https://github.com/moov-io/watchman/issues/756

--- pkg/address/address_test.go
+++ pkg/address/address_test.go
@@ -1,3 +1,19 @@
-//go:build !libpostal
-
 package address
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseAddress(t *testing.T) {
+	input := "123 First St Anytown CA 90210"
+
+	got := ParseAddress(context.Background(), input)
+	require.Equal(t, "123 first st", strings.ToLower(got.Line1))
+	require.Equal(t, "anytown", strings.ToLower(got.City))
+	require.Equal(t, "90210", strings.ToLower(got.PostalCode))
+	require.Equal(t, "ca", strings.ToLower(got.State))
+}
